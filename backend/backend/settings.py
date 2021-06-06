fb28ff8660c04c48b3f75fca8611b8f8e3ba76e6
noahsim222 <noahsim222@gmail.com>
2021-06-06 22:42:16 +0530

    Custom User Model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../frontend')],
+        'DIRS': [os.path.join(BASE_DIR, '../frontend/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
