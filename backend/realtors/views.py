0636f5b9351408713bf27570898b87aceae279f5
noahsim222 <noahsim222@gmail.com>
2021-06-08 17:17:15 +0530

    Realtors Views

--- backend/realtors/views.py
+++ backend/realtors/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import permissions
+from .models import Realtor
+from .serializers import RealtorSerializer
 
 # Create your views here.
+
+# Getting a List of Realtors
+class RealtorListView(ListAPIView):
+    permission_classes = (permission.AllowAny, )
+    queryset = Realtor.objects.all()
+    serializer_class = RealtorSerializer
+    pagination_class = None  #Default list will be paginated
+
+
+
+# Retrieving a single Realtor by Id
+class RealtorView(RetrieveAPIView):
+    queryset = Realtor.objects.all()
+    serializer_class = RealtorSerializer
