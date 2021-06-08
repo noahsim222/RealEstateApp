7166cae1d635d83650ccc9abfc47016177699140
noahsim222 <noahsim222@gmail.com>
2021-06-08 16:54:44 +0530

    Realtors app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'corsheaders',
-    'accounts'
+
+    'accounts',
+    'realtors'
 ]
 
 MIDDLEWARE = [
