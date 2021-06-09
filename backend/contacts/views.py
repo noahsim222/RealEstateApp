2873aab7dfd9c570e6541c5c1abe562b9ad601d5
noahsim222 <noahsim222@gmail.com>
2021-06-09 08:23:11 +0530

    Contacts View

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.views import APIView
+from .models import Contact
+from django.core.mail import send_mail
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class ContactCreateView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    def post(self, request, format=None):
+        data = self.request.data
+
+        try:
+            send_mail(
+                data['subject'],
+                'Name: '
+                +data['name']
+                +'\nEmail:' 
+                +data['email']
+                + '\n\nMessage: \n'
+                +data['message']
+                'aakriti1435@gmail.com',
+                ['aakritigupta1435@gmail.com'],
+                fail_silently=False
+            )
+
+            contact = Contact(
+                name=data['name'],
+                email=data['email'],
+                subject=data['subject'],
+                message=data['message'],
+            )
+            contact.save()
+
+            return Response({'success': 'Message sent successfully'})
+
+        except:
+            return Response({'error': 'Message Failed to Send'})
+
+
+
+
+
+
