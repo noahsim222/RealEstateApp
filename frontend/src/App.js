b5aed61e27c8a1f02f70a2f4458dc3fc828cc994
noahsim222 <noahsim222@gmail.com>
2021-06-10 21:46:09 +0530

    Frontend Redux Store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,28 +10,33 @@ import Login from "./containers/Login";
 import SignUp from "./containers/SignUp";
 import NotFound from "./components/NotFound";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./sass/main.scss";
 
 function App() {
 	return (
-		<Router>
-			<Layout>
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/about" component={About} />
-					<Route exact path="/contact" component={Contact} />
-					<Route exact path="/listings" component={Listings} />
-					<Route
-						exact
-						path="/listings/:id"
-						component={ListingDetail}
-					/>
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/signup" component={SignUp} />
-					<Route component={NotFound} />
-				</Switch>
-			</Layout>
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<Layout>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/contact" component={Contact} />
+						<Route exact path="/listings" component={Listings} />
+						<Route
+							exact
+							path="/listings/:id"
+							component={ListingDetail}
+						/>
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/signup" component={SignUp} />
+						<Route component={NotFound} />
+					</Switch>
+				</Layout>
+			</Router>
+		</Provider>
 	);
 }
 
