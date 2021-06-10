ced320c377c0650fd42bb27f8c0e58cd7dff57f3
noahsim222 <noahsim222@gmail.com>
2021-06-10 19:00:35 +0530

    Frontend Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Layout from "./hocs/Layout";
+import Home from "./containers/Home";
+import About from "./containers/About";
+import Contact from "./containers/Contact";
+import Listings from "./containers/Listings";
+import ListingDetail from "./containers/ListingDetail";
+import Login from "./containers/Login";
+import SignUp from "./containers/SignUp";
 
 function App() {
-	return <div>App</div>;
+	return (
+		<Router>
+			<Layout>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/about" component={About} />
+					<Route exact path="/contact" component={Contact} />
+					<Route exact path="/listings" component={Listings} />
+					<Route
+						exact
+						path="/listings/:id"
+						component={ListingDetail}
+					/>
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/signup" component={SignUp} />
+				</Switch>
+			</Layout>
+		</Router>
+	);
 }
 
 export default App;
